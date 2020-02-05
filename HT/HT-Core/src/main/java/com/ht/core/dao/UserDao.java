52993d8baf9a905d9492b778d02c63697814d48e
kadrivaripuu <kadri.varipuu@gmail.com>
2020-02-05 16:53:36 +0200

    TestFind, TestSave, TestDelete

--- HT/HT-Core/src/main/java/com/ht/core/dao/UserDao.java
+++ HT/HT-Core/src/main/java/com/ht/core/dao/UserDao.java
@@ -3,9 +3,14 @@ package com.ht.core.dao;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.ht.core.model.User;
+
 
 @Repository
 public interface UserDao extends JpaRepository<User,Long> {
@@ -13,5 +18,11 @@ public interface UserDao extends JpaRepository<User,Long> {
 	List<User> findByUsername(String username);
 	
 	List<User> findByUsernameAndRole(String username, String role);
+
+	@Transactional
+	void deleteByUsername(String username);
+	
+//	@Query("from user u where u.fname=:fname")
+//	List<User> findByFname(@Param("fname") String fname);
 
 }
--- HT/HT-Core/src/test/java/com/ht/core/dao/intgr/test/UserDaoIntTest.java
+++ HT/HT-Core/src/test/java/com/ht/core/dao/intgr/test/UserDaoIntTest.java
@@ -1,34 +1,94 @@
 package com.ht.core.dao.intgr.test;
 
+import javax.sql.DataSource;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import com.ht.core.config.HtCoreConfig;
 import com.ht.core.dao.UserDao;
 import com.ht.core.model.User;
+import com.zaxxer.hikari.HikariDataSource;
 
 public class UserDaoIntTest {
 
+	private AnnotationConfigApplicationContext context;
+
+	@Before
+	public void init() {
+		context = new AnnotationConfigApplicationContext(HtCoreConfig.class);
+	}
+
+	
 	@Test
 	public void testSave() {
+		UserDao userDao = context.getBean(UserDao.class);
+		userDao.save(mockUser());
+	}
 
-		ApplicationContext context = new AnnotationConfigApplicationContext(HtCoreConfig.class);
+	@Test
+	public void testFind() {
 		UserDao userDao = context.getBean(UserDao.class);
 
-		// 1) Create UserDto
+		String usernameToTest = "järgmineKasutaja";
+		String roleToTest = "järgmineKasutaja";
+		
+		User user = mockUser();
+		user.setUsername(usernameToTest);
+		user.setRole(roleToTest);
+		
+		userDao.findByUsername(usernameToTest);
+		userDao.findByUsernameAndRole(usernameToTest, roleToTest);
+	}
+	
+//	@Test
+//	public void testFindByQuery() {
+//		UserDao userDao = context.getBean(UserDao.class);
+//		
+//		String fnameToTest = "NextOne";
+//		User user = mockUser();
+//		user.setFname(fnameToTest);
+//		
+//		userDao.findByFname(fnameToTest);
+//	}
+
+	@Test
+	public void testDelete() {
+		UserDao userDao = context.getBean(UserDao.class);
+
+		String usernameToTest = "nextOneUser";
+		
+		User user = mockUser();
+		user.setUsername(usernameToTest);
+		userDao.save(user);
+
+//		userDao.deleteById(userDao.findAll().get(0).getId());
+		userDao.deleteByUsername(usernameToTest);
+	}
+
+	
+	
+	
+	@After
+	public void endTest() {
+		HikariDataSource ds = (HikariDataSource) context.getBean(DataSource.class);
+		ds.close();
+		context.close();
+	}
+
+	private User mockUser() {
+
 		User mockUser = new User();
 		mockUser.setFname("Next");
 		mockUser.setLname("User");
 		mockUser.setUsername("nextUser");
 		mockUser.setPassword("tere");
 		mockUser.setEmail("next@user.com");
-//		mockUser.setRole("kasutaja");
+		mockUser.setRole("kasutaja");
 
-		userDao.save(mockUser);
-		userDao.findByUsername("järgmineKasutaja");
-		userDao.findByUsernameAndRole("järgmineKasutaja", "kasutaja");
+		return mockUser;
 	}
-
 
 }
