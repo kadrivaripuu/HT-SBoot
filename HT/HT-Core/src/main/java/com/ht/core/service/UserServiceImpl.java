015d5b20505b18634af60bb921f676e6642fe055
kadrivaripuu <kadri.varipuu@gmail.com>
2020-02-07 16:35:25 +0200

    get AllUsers  (and delete?)

--- HT/HT-Core/src/main/java/com/ht/core/service/UserServiceImpl.java
+++ HT/HT-Core/src/main/java/com/ht/core/service/UserServiceImpl.java
@@ -1,9 +1,13 @@
 package com.ht.core.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import com.ht.core.beans.UserDto;
 import com.ht.core.dao.UserDao;
@@ -15,47 +19,47 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Service
 @Transactional(propagation = Propagation.REQUIRED)
-public class UserServiceImpl  {
+public class UserServiceImpl {
 
 	@Autowired
 	private UserDao userDao;
-	
-	
-	
-	public void register(UserDto userDto) {		
-		//let's imagine (8 lines of code) many checks before save...
+
+	public void register(UserDto userDto) {
+		// let's imagine (8 lines of code) many checks before save...
 		User userAsModel = BeanUtils.dto2Model(userDto);
-		userDao.save(userAsModel);		
+		userDao.save(userAsModel);
 	}
 
-		
 	public UserDto findById(long id) {
 		User user = userDao.findById(id).get();
 		UserDto userAsDto = BeanUtils.model2Dto(user);
 		return userAsDto;
 	}
-	
-	
+
+	public List<User> findAllUsers() {
+		List<User> allUsers = new ArrayList<User>();
+		for (int i = 0; i < userDao.findAll().size(); i++) {
+			allUsers.add(userDao.findAll().get(i));
+		}
+		return allUsers;
+	}
+
 	public void deleteUser(long id) {
 		userDao.deleteById(id);
 	}
-	
-	
+
 	/*
-	
-	@Override
-	public void updateUser(UserDto newUserDto)  throws IllegalStateException {
-
-		if (newUserDto.getId() == null) {
-			throw new IllegalStateException("User not found!");
-		}
-
-		User user = BeanUtils.dto2Model(userDto);
-		userDao.updateUser(user);
-
-		
-//		throw new UnsupportedOperationException("Not implemented");
-	}
-
-*/
+	 * 
+	 * @Override public void updateUser(UserDto newUserDto) throws
+	 * IllegalStateException {
+	 * 
+	 * if (newUserDto.getId() == null) { throw new
+	 * IllegalStateException("User not found!"); }
+	 * 
+	 * User user = BeanUtils.dto2Model(userDto); userDao.updateUser(user);
+	 * 
+	 * 
+	 * // throw new UnsupportedOperationException("Not implemented"); }
+	 * 
+	 */
 }
--- HT/HT-Web/src/main/java/com/ht/web/controllers/UserRestController.java
+++ HT/HT-Web/src/main/java/com/ht/web/controllers/UserRestController.java
@@ -1,5 +1,7 @@
 package com.ht.web.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.ht.core.beans.UserDto;
+import com.ht.core.model.User;
 import com.ht.core.service.UserServiceImpl;
 import com.ht.web.beans.ResponseBean;
 
@@ -21,29 +24,35 @@ public class UserRestController {
 
 	@Autowired
 	private UserServiceImpl userService;
+
 	
-	 @RequestMapping(value = "/save", method = RequestMethod.POST,
-	            produces = MediaType.APPLICATION_JSON_VALUE,
-	            consumes= MediaType.APPLICATION_JSON_VALUE)
-	    public @ResponseBody
-	    ResponseBean<String> moduleProcess(@RequestBody UserDto userDto) {
-	        userService.register(userDto);
-	        return new ResponseBean<>("User Saved");
-	    }
+	@RequestMapping(value = "/all", method = RequestMethod.GET, 
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody List<User> findAll() {
+		List users = (List<User>) userService.findAllUsers();
+		return users;
+		
+	}
 	
-	@RequestMapping(value = "/{id}", method = RequestMethod.GET,
-            produces = MediaType.APPLICATION_JSON_VALUE)
-    public @ResponseBody
-    ResponseBean<UserDto> find(@PathVariable Long id) {
-       UserDto userDto = userService.findById(id);
-        return new ResponseBean<UserDto>(userDto);
-    }
-	
-	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET,
-            produces = MediaType.APPLICATION_JSON_VALUE)
-    public @ResponseBody
-    ResponseBean<UserDto> userDelete(@PathVariable Long id) {
-        userService.deleteUser(id);
-        return new ResponseBean<>("User Deleted");
-    }
+	@RequestMapping(value = "/save", method = RequestMethod.POST, 
+			produces = MediaType.APPLICATION_JSON_VALUE, 
+			consumes = MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody ResponseBean<String> moduleProcess(@RequestBody UserDto userDto) {
+		userService.register(userDto);
+		return new ResponseBean<>("User Saved");
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET, 
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody ResponseBean<UserDto> find(@PathVariable Long id) {
+		UserDto userDto = userService.findById(id);
+		return new ResponseBean<UserDto>(userDto);
+	}
+
+	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET, 
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody ResponseBean<UserDto> userDelete(@PathVariable Long id) {
+		userService.deleteUser(id);
+		return new ResponseBean<>("User Deleted");
+	}
 }
